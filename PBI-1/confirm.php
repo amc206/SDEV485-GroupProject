2d0bf97e6b66dfa0bb20e403b412d30c77969ee4
Angel Campos <campos.angel@student.greenriver.edu>
2023-01-15 21:12:29 -0500

    updated the print button look and style

--- PBI-1/confirm.php
+++ PBI-1/confirm.php
@@ -9,6 +9,9 @@ error_reporting(E_ALL);
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="styles/PBI-1-styles.css">
     <title>Confirmation</title>
@@ -19,29 +22,33 @@ error_reporting(E_ALL);
 <div id ="main" class ="container">
     <div class="jumbotron">
         <h2 class="display-4"><center><b>Student Plan</b></center></h2>
-        <p><code id="output"></code></p>
+        <!--<p><code id="output"></code></p>-->
+
+        <div id="print">
+            <!--<button onclick="window.print()">Print</button>-->
+            <a class="btn btn-info btn-lg" onclick="window.print()">
+            <span class="glyphicon glyphicon-print">Print</span>
+            </a>
+        </div>
     </div>
 
-    <div id="print">
-        <button onclick="window.print()">Print</button>
-    </div>
 
 
-<?php
+    <?php
 
-$fall = $_GET['fall'];
-$winter = $_GET['winter'];
-$spring = $_GET['spring'];
-$summer = $_GET['summer'];
+    $fall = $_GET['fall'];
+    $winter = $_GET['winter'];
+    $spring = $_GET['spring'];
+    $summer = $_GET['summer'];
 
-echo "<p><b>Fall:</b> $fall</p>";
-echo "<p><b>Winter:</b> $winter</p>";
-echo "<p><b>Spring:</b> $spring</p>";
-echo "<p><b>Summer:</b> $summer</p>";
+    echo "<p><b>Fall:</b> $fall</p>";
+    echo "<p><b>Winter:</b> $winter</p>";
+    echo "<p><b>Spring:</b> $spring</p>";
+    echo "<p><b>Summer:</b> $summer</p>";
 
 
 
-?>
+    ?>
 
 </div>
 <script src="scripts/PBI-1-scripts.js"></script>
